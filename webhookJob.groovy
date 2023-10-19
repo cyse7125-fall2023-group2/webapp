c2af725f1b73f06b465f4f6998e708f2fb7b3d4e
shikha-commits <singh.shikha@northeastern.edu>
2023-10-19 14:48:51 -0400

    Adding Job DSL file

---
+++ webhookJob.groovy
@@ -0,0 +1,28 @@
+pipelineJob('webapp') {
+    description('My Pipeline Job Description')
+    triggers {
+          hudsonStartupTrigger {
+            nodeParameterName("main")
+            label("main")
+            quietPeriod("0")
+            runOnChoice("False")
+          }
+    }
+    definition {
+        cpsScm {
+            scriptPath('Jenkinsfile') // Reference the Jenkinsfile in your SCM
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/cyse7125-fall2023-group2/webapp.git')
+                        credentials('GITHUB_CREDENTIALS_ID') // Specify your GitHub credentials ID
+                    }
+                    branch('main') // Specify the branch you want to build
+                }
+            }
+        }
+    }
+    triggers {
+        githubPush() // Trigger the job on a GitHub push event
+    }
+}
