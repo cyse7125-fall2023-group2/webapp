04fc3bb8c22c91fefc60cd09dd375224b5d0d76b
shikha-commits <singh.shikha@northeastern.edu>
2023-10-19 23:53:02 -0400

    webhook job

--- webhookJob.groovy
+++ webhookJob.groovy
@@ -1,13 +1,10 @@
 pipelineJob('webapp-pipeline') {
     description('My Pipeline Job Description')
-    triggers {
-        githubPush() // Trigger the job on a GitHub push event
-    }
     definition {
         cpsScm {
             scriptPath('Jenkinsfile') // Reference the Jenkinsfile in your SCM
             scm {
-                github {
+                git {
                     remote {
                         url('https://github.com/cyse7125-fall2023-group2/webapp.git')
                         credentials('WEBHOOK_CREDENTIAL') // Specify your GitHub credentials ID
