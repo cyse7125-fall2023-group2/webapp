b0a36fa4978801e5e123f1ba90e9ebccf49072f2
Ankit <ankitlall@Ankits-MacBook-Pro.local>
2023-10-22 19:29:04 -0400

    app change

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-const express=require('express'); 
-const path=require('path'); 
-const bodyParser = require('body-parser');
-const httpCheckRoutes = require('./api_routes/httpCheck');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const httpCheckRoutes = require("./api_routes/httpCheck");
 
 //Database
-const {sequelize} = require('./models')
-const app=express(); 
-const fs = require('fs');
+const { sequelize } = require("./models");
+const app = express();
+const fs = require("fs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/healthz',async(req,res)=>{
+app.get("/healthz", async (req, res) => {
+  res.setHeader("cache-control", "no-cache");
+  if (req.method === "GET" && Object.keys(req.query).length > 0) {
+    return res.status(400).send();
+  }
+  if (req.method === "GET" && Object.keys(req.body).length > 0) {
+    return res.status(400).send();
+  }
 
-    res.setHeader('cache-control','no-cache')
-    if (req.method === 'GET' && Object.keys(req.query).length > 0) {
-       return res.status(400).send();
-    }
-    if (req.method === 'GET' && Object.keys(req.body).length >0) {
-       return res.status(400).send() ;   
-    }
-
-    sequelize
+  sequelize
     .authenticate()
     .then(() => {
       // Database connection is successful
@@ -29,19 +28,19 @@ app.get('/healthz',async(req,res)=>{
     })
     .catch((err) => {
       // Database connection failed
-      console.error('Database connection failed:', err);
+      console.error("Database connection failed:", err);
       res.status(503).end(); // HTTP 503 Service Unavailable
     });
-  })
+});
 
-app.all('/healthz', async(req,res)=>{
-  res.setHeader('cache-control','no-cache')
+app.all("/healthz", async (req, res) => {
+  res.setHeader("cache-control", "no-cache");
   res.status(405).send();
-})
+});
 
-app.use('/v1/http-check',httpCheckRoutes);
-    
-const PORT=process.env.APP_PORT || 8080;
-app.listen({ port : PORT},async()=>{  })
+// app.use('/v1/http-check',httpCheckRoutes);
 
-module.exports = app
+const PORT = process.env.APP_PORT || 4000;
+app.listen({ port: PORT }, async () => {});
+
+module.exports = app;
